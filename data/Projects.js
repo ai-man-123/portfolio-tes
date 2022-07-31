1659bf092e0268136cf3032d7a5a8d9b521d8e03
RizFurr <109797310+RizzyDev81@users.noreply.github.com>
2022-07-31 14:16:08 +0800

    Update Projects.js

--- data/Projects.js
+++ data/Projects.js
@@ -1,11 +1,10 @@
 export const Projects = [
   {
     id: 1,
-    name: 'Space Game Community ID',
-    slug: 'sgcid',
-    img: 'sgcid',
-    demo: 'https://sgcid.mhdrijal.my.id',
-    sourceCode: 'https://github.com/euxzy/sgcid',
+    name: 'Web Downloader',
+    slug: 'downloader',
+    img: 'downloader',
+    demo: 'https://downloader.rizfurr.ml',
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla alias accusantium commodi quia rerum reprehenderit similique, ipsa cupiditate vel voluptates sapiente, unde ducimus dignissimos saepe consequuntur possimus maxime voluptatibus laudantium.',
   },
@@ -21,26 +20,17 @@ export const Projects = [
   },
   {
     id: 3,
-    name: 'Jadwal Sholat',
-    slug: 'jadwal-sholat',
-    img: 'jadwal-sholat',
-    demo: 'https://jadwal-sholat.mhdrijal.my.id',
-    sourceCode: 'https://github.com/euxzy/jadwal-sholat',
+    name: 'File Uploader',
+    slug: 'fileuploader',
+    img: 'fileuploader',
+    demo: 'https://file.rizfurr.ml',
+    sourceCode: 'https://github.com/rizzydev81/file-uploader',
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla alias accusantium commodi quia rerum reprehenderit similique, ipsa cupiditate vel voluptates sapiente, unde ducimus dignissimos saepe consequuntur possimus maxime voluptatibus laudantium.',
   },
+ 
   {
     id: 4,
-    name: 'Warung Sunda',
-    slug: 'warung-sunda',
-    img: 'warung-sunda',
-    demo: 'https://',
-    sourceCode: 'https://github.com/euxzy/warung-sunda',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla alias accusantium commodi quia rerum reprehenderit similique, ipsa cupiditate vel voluptates sapiente, unde ducimus dignissimos saepe consequuntur possimus maxime voluptatibus laudantium.',
-  },
-  {
-    id: 5,
     name: 'Coming Soon Project',
     slug: 'coming-soon-project',
     img: 'coming-soon',
