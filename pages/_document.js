c7231007b3ec695fa4d543ba6d74c430af9fe40a
RizFurr <rizzowohq@gmail.com>
2022-09-16 21:03:34 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,15 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
+
+const image = "https://i.ibb.co/FWGHJYr/IMG-20220916-202231.jpg";
+const description = "My Website";
 const Document = () => {
   return (
     <Html lang="en">
       <Head>
-        <meta name="description" content="My Portfolio" />
+        <meta property="og:image" content={image}/>
+        <meta property="og:description" content={description}/>
+        <meta name="description" content="Hewoo My Name Is Rizz, Nice To Meet U" />
         <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
         <meta httpEquiv="X-UA-Compatible" content="IE=7" />
