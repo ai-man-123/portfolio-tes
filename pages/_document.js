914e8b0952f305f7f785a3ba1e02a94d4f210055
AIMAN *͘⁴̅⁰͍⁴̵〆* <Lynxaiman@yahoo.com>
2022-12-09 21:24:45 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
 
-const image = "https://i.ibb.co/FWGHJYr/IMG-20220916-202231.jpg";
+const image = "https://filezone.my.id/file/k008yn0mngqy4k6fk8sm.jpg";
 const description = "My Website";
 const Document = () => {
   return (
@@ -9,7 +9,7 @@ const Document = () => {
       <Head>
         <meta property="og:image" content={image}/>
         <meta property="og:description" content={description}/>
-        <meta name="description" content="Hewoo My Name Is Rizz, Nice To Meet U" />
+        <meta name="description" content="Hewoo My Name Is Aiman, Nice To Meet U" />
         <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
         <meta httpEquiv="X-UA-Compatible" content="IE=7" />
