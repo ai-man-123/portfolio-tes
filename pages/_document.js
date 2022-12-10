2859112af7ef8179305291b43f8f1d81990674c4
AIMAN *͘⁴̅⁰͍⁴̵〆* <Lynxaiman@yahoo.com>
2022-12-10 10:53:33 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
 
-const image = "https://i.ibb.co/fYf4q6D/IMG-20220916-202231.jpg";
-const description = "My Website";
 const Document = () => {
   return (
     <Html lang="en">
       <Head>
-        <meta property="og:image" content={image}/>
-        <meta property="og:description" content={description}/>
-        <meta name="description" content="Hewoo My Name Is Aiman, Nice try. " />
+        <meta name="description" content="My Portfolio" />
         <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
         <meta httpEquiv="X-UA-Compatible" content="IE=7" />
